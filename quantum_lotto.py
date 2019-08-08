223904505f2e5a96f12049deb0f7069344ab330f
giulio <giuliop@gmail.com>
2019-08-08 22:44:18 +0200

    generate combination from file

--- quantum_lotto.py
+++ quantum_lotto.py
@@ -2,18 +2,24 @@ import argparse
 from itertools import combinations
 
 parser = argparse.ArgumentParser()
-parser.add_argument("binary", help="a binary string indicating a series of universe splits :)")
+parser.add_argument("filename",
+        help="a file containing a series of universe splits as lines of 0 and 1 :)")
 args = parser.parse_args()
 
 numbers = range(1,91)
 plays = combinations(numbers, 6)
 max = 622614630
 
-n = int(args.binary, 2) % max
+with open(args.filename, 'r') as f:
+    n = f.read().replace('\n', '')
+
+n = '000100011010111101011000010111'
+n = int(n, 2) % max
 
 for i, p in enumerate(plays):
     if i == n:
         print('\rThe winning combinations is: ' + " ".join([str(x) for x in p]))
+        print('\n')
         break
 
     if (i % 1000000) == 0:
